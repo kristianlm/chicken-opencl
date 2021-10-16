b4bdd38bceef24e322dde8dbd08f1b1442667bcb
Kristian Lein-Mathisen <kristianlein@gmail.com>
2021-10-16 12:49:43 +0200

    compat: use uint instead of ulong in tests
    because I ran this on my phone, and the kernel won't compile :-( I
    changed ulong to uint and that worked. That's pretty dumb, but our
    test suite shouldn't rely on anything "fancy". Although I don't really
    understand how it's possible to be that un-compliant.

--- tests/run.scm
+++ tests/run.scm
@@ -6,16 +6,16 @@
   (define context (context-create device))
   (define cq (command-queue context device))
 
-  (define data (u64vector 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15))
+  (define data (u32vector 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15))
   ;;                                 ,-- for type and size information only
   (define A (buffer-create context data))
   (buffer-write A cq data)
   (test "read-back after buffer-write"
-        (u64vector 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+        (u32vector 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
         (buffer-read A cq))
 
   (define kernel (kernel-create (program-build (program-create context "
-__kernel void tt(__global ulong *A) {
+__kernel void tt(__global uint *A) {
   uint gx = get_global_id(0) - get_global_offset(0);
   uint gy = get_global_id(1) - get_global_offset(1);
   uint lx = get_local_id(0);
@@ -30,7 +30,7 @@ __kernel void tt(__global ulong *A) {
 
   (kernel-enqueue kernel cq (list 4 4) local-work-sizes: (list 1 1))
   (test "simple kernel 4x4 1x1"
-        (u64vector 1000000000 1000000001 1000000002 1000000003
+        (u32vector 1000000000 1000000001 1000000002 1000000003
                    1000000010 1000000011 1000000012 1000000013
                    1000000020 1000000021 1000000022 1000000023
                    1000000030 1000000031 1000000032 1000000033)
@@ -38,7 +38,7 @@ __kernel void tt(__global ulong *A) {
 
   (kernel-enqueue kernel cq (list 4 4) local-work-sizes: (list 2 2))
   (test "simple kernel 4x4 2x2"
-        (u64vector 1000000000 1000001001 1000000002 1000001003
+        (u32vector 1000000000 1000001001 1000000002 1000001003
                    1000010010 1000011011 1000010012 1000011013
                    1000000020 1000001021 1000000022 1000001023
                    1000010030 1000011031 1000010032 1000011033)
@@ -47,7 +47,7 @@ __kernel void tt(__global ulong *A) {
   (kernel-enqueue kernel cq (list 4 4) local-work-sizes: (list 1 1) global-work-offsets: (list 1 3))
   (test "simple kernel 4x4 1x1 with 3x3 offset"
         ;;      oo ll gg   oo ll gg   oo ll gg   oo ll gg
-        (u64vector 1013000000 1013000001 1013000002 1013000003
+        (u32vector 1013000000 1013000001 1013000002 1013000003
                    1013000010 1013000011 1013000012 1013000013
                    1013000020 1013000021 1013000022 1013000023
                    1013000030 1013000031 1013000032 1013000033)
